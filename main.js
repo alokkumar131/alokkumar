ad9a2ab5c7bdabcbbcdd8407097dd2b5a4edac06
alokkumar131 <dmalok8@gmail.com>
2019-12-29 13:28:59 -0800

    first commit

--- main.js
+++ main.js
@@ -14,6 +14,30 @@ function openNav() {
   }
 
   $(function() {
+      $("#myCarousel").on("slide.bs.carousel", function(e) {
+        var $e = $(e.relatedTarget);
+        var idx = $e.index();
+        var itemsPerSlide = 3;
+        var totalItems = $(".carousel-item").length;
+    
+        if (idx >= totalItems - (itemsPerSlide - 1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i = 0; i < it; i++) {
+            // append slides to end
+            if (e.direction == "right") {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            } else {
+              $(".carousel-item")
+                .eq(0)
+                .appendTo($(this).find(".carousel-inner"));
+            }
+          }
+        }
+      });
+    
+    
     $('.scroll').on('click', function(e) {
       e.preventDefault();
       var hash = this.hash;
@@ -49,4 +73,5 @@ function openNav() {
 
     
   });
+  
   
